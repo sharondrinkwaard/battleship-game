fc6cdb73266f5c45ffcda6f455e4b352d0bc21d7
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-06-28 12:18:42 +0000

    Create main +  game board functions

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def create_player_board():
+    '''
+    Prints the battleship board for the user
+    On the user board is shown where his battleships are
+    On the computer's board these are hidden for the user
+    '''
+    print('This is the player board')
+
+
+def create_computer_board():
+    '''
+    Prints the battleship board of the computer
+    '''
+    print('This is the computer board')
+
+
+def main():
+    '''
+    Main function where all other functions are being called from.
+    This functions runs the game
+    '''
+    print("Welcome to the Battleship game!")
+    print("")
+    create_computer_board()
+    create_player_board()
+
+
+main()
