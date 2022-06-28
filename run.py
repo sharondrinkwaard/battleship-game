fc8a160e0a6ee03a0d86b9a32c5f03d12ae4b8bf
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-06-28 12:29:06 +0000

    Add function to display game rules

--- run.py
+++ run.py
@@ -15,13 +15,24 @@ def create_computer_board():
     print('This is the computer board')
 
 
+def game_rules():
+    '''
+    Displays the game rules
+    '''
+    rules_answer = input("Would you like to read the rules? y/n \n")
+    if rules_answer == 'y':
+        print('These are the rules')
+
+
 def main():
     '''
     Main function where all other functions are being called from.
     This functions runs the game
     '''
-    print("Welcome to the Battleship game!")
-    print("")
+    print("Welcome to the Battleships game!")
+    game_rules()
+
+
     create_computer_board()
     create_player_board()
 
